3e1df9365a5275d3047c0b93769149e97886cca4
Robert Jackson <73659092+KumaUrs@users.noreply.github.com>
2022-05-09 15:05:44 -0400

    What up

--- JavaSudoku/src/MainGame.java
+++ JavaSudoku/src/MainGame.java
@@ -1,18 +1,18 @@
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
-
+// nerdd
 public class MainGame extends JFrame{
-	
+
 	public MainGame() {
 		super("Gonq Sudoku");
 		setLayout(new BorderLayout());
-		
+
 		setSize(800, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);	
+		setVisible(true);
 		System.out.println("Test Commit");
 	}
-	
-	
+
+
 }
